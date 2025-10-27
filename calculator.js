2872d7708fe782a3928eb7a806e0c7b4ea306b00
Victor Ugochukwu <victorugochukwunwankwo@gmail.com>
2025-10-27 14:14:13 +0100

    Clean up duplicate variables and confirm working calculator logic

--- calculator.js
+++ calculator.js
@@ -13,19 +13,6 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(operator, a, b) {
-  a = Number(a);
-  b = Number(b);
-
-switch (operator) {
-    case '+': return add(a, b);
-    case '-': return subtract(a, b);
-    case '*': return multiply(a, b);
-    case '/': return divide(a, b);
-    default: return null;
-  }
-}
-
 // Operate function
 function operate(operator, a, b) {
   a = Number(a);
